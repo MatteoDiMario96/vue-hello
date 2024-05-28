0874849f9067d0453bbfa16cf3c09fcf48f85b39
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-28 12:55:25 +0200

    Aggiunta grazie alla funzione v-bind non contratta all src la variabile,propieta(?).. immagine assegnata in data.

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            immagine: "https://i.redd.it/3fi2a5ae1hkz.jpg"
         }
     }
 }).mount('#app')
