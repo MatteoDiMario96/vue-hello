7fed097256ba88eaf52eb278a986cbe7ae0e7a2f
MatteoDiMario96 <dm.matteo96@live.it>
2024-05-28 22:25:19 +0200

    Un po di divertimento con il bonus. Non  sono riuscito a dare argomenti alla mia funzione.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,23 @@ createApp({
     data() {
         return {
             message: 'Hello Vue!',
-            immagine: "https://i.redd.it/3fi2a5ae1hkz.jpg"
+            immaginePrincipale: "https://i.redd.it/3fi2a5ae1hkz.jpg",
+            immagineSecondaria: "https://web-savvy-marketing.com/wp-content/uploads/2016/10/Crazy-Website-Developers.jpg"
+        }
+    },
+    methods: {
+        supplica (){
+            alert("Mamma mia, sei un programmatore!!")
+            let risultatoSupplica = false;
+            while(!risultatoSupplica){
+            let rispostaUtenteSupplica =  prompt("Dimmi di si, ti prego!");
+            rispostaUtenteSupplica = rispostaUtenteSupplica.toLowerCase();
+                if(rispostaUtenteSupplica === "si" || rispostaUtenteSupplica === "yes" || rispostaUtenteSupplica === "certo"){
+                    risultatoSupplica = true;
+                }else{
+                    alert("Guarda che me ne accorgo, fai il bravo o la brava, perfavore!")
+                }
+            }
         }
     }
 }).mount('#app')
